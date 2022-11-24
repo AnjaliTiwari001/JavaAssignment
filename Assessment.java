44f1d6efe537d402ae603ddb900f0ee1b616ec59
AnjaliTiwari001 <104716168+AnjaliTiwari001@users.noreply.github.com>
2022-11-24 15:56:47 +0530

    Add files via upload

---
+++ Assessment.java
@@ -0,0 +1,10 @@
+public class Assessment {
+    public static void main(String[] args) {
+        String givenstring = "Full stack developer";
+        System.out.println('"' +givenstring.toUpperCase()+ '"');
+        System.out.println(givenstring.replace(" ",""));
+        System.out.println(givenstring.replace("v","w"));
+        System.out.println(givenstring.indexOf('u'));
+        System.out.println(givenstring.indexOf('v'));
+    }
+}
