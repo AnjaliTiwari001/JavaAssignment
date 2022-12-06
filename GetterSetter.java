e79894ba5765bed477f33b32303bd84f7c6ac3b2
AnjaliTiwari001 <104716168+AnjaliTiwari001@users.noreply.github.com>
2022-12-06 12:11:30 +0530

    Add files via upload

---
+++ GetterSetter.java
@@ -0,0 +1,9 @@
+public class class2 extends student {
+    public static void main(String[] args) {
+    student s1= new student();
+    s1.setName("Anjali");
+    System.out.println(s1.getName());
+
+
+    }
+}
